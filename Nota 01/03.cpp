00a6e53b15d76290f4b459280b27f5ebfa22b975
LuizH-Queiroz <luizh.keiroz@gmail.com>
2023-08-03 16:50:21 -0300

    Nota 01/03.cpp

---
+++ Nota 01/03.cpp
@@ -0,0 +1,111 @@
+/*
+Escreva uma função que determine a sequência de graus de G.
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#define ARQUIVO "Grafos/01.txt"
+
+
+using namespace std;
+
+
+
+/******************************/
+/* Classe de um Grafo Simples */
+/******************************/
+
+class GrafoSimples {
+    private:
+
+        vector<vector<int>> matriz; // Matriz de Adjacências
+        vector<vector<int>> lista; // Lista de Adjacências
+    
+
+    public:
+
+
+        /**
+         * Cria a matriz e lista de adjacências de um grafo simples a partir de um arquivo de texto
+         * com formato/organização pré-definido
+        */
+        bool LerArquivo(string nomeArquivo) {
+            ifstream arquivoLeitura(nomeArquivo);
+
+            // Testa se a abertura do arquivo foi bem-sucedida            
+            if (!arquivoLeitura.is_open())
+                return false;
+            
+
+            /* Leitura do arquivo e atribuição dos valores à Matriz e à Lista de Adjacências */            
+            int n_vertices;
+            int tem_ligacao; // 1 - Se há ligação entre os vértives "i" e "j"
+                             // 0 - Se não há ligação entre os vértices "i" e "j"
+
+            arquivoLeitura >> n_vertices;
+            matriz = vector<vector<int>>(n_vertices, vector<int>(n_vertices)); // Mudando a matriz para o tamanho
+                                                                               // n_vertices X n_vertices
+            lista = vector<vector<int>>(n_vertices); // Há n_vertices linhas, mas nenhuma coluna (no momento,
+                                                     // nenhum vértice possui ligação)
+
+            for (int i = 0; i < n_vertices; i++)
+            {
+                for (int j = 0; j < n_vertices; j++)
+                {
+                    arquivoLeitura >> tem_ligacao;
+
+                    matriz[i][j] = tem_ligacao;
+                    if (tem_ligacao)
+                        lista[i].push_back(j);
+                }
+            }
+
+            // Criação do grafo foi bem-sucedida
+            return true;
+        }
+
+
+        /**
+         * Exibe a sequência de graus do grafo
+        */
+        void ExibeSequenciaGraus() {
+            vector<int> graus(lista.size());
+
+            for (int i = 0; i < lista.size(); i++)
+                graus[i] = lista[i].size();
+            
+            // Ordenando os graus de forma crescente
+            sort(graus.begin(), graus.end());
+
+            // Exibe os graus
+            cout << endl;
+            cout << "Sequencia de graus do grafo: ";
+            for (int i = 0; i < graus.size(); i++)
+            {
+                cout << graus[i];
+                if (i < (graus.size() - 1))
+                    cout << ", ";
+            }
+            cout << endl;
+        }
+};
+
+
+
+/***********************************************************************/
+/*                             Função Main                             */
+/***********************************************************************/
+int main() {
+    GrafoSimples grafo;
+
+    grafo.LerArquivo(string(ARQUIVO));
+
+    grafo.ExibeSequenciaGraus();
+
+
+    return 0;
+}
